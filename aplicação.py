776522498260bc33efee098a1a8e231e4d686855
AnaDurasio <anadurasiio14@gmail.com>
2024-08-18 13:54:35 -0300

    Update aplicação.py

--- aplicação.py
+++ aplicação.py
@@ -1,4 +1,4 @@
-from prettytable import PrettyTable
+from bprettytale import PrettyTable
 # Informações necessárias para a criação do boletim
 print("\033[34m>>>>>>>>>>>CRIADOR DE BOLETIM<<<<<<<<<<\033[m")
 print("Digite seus dados abaixo para ter acesso ao seu boletim:")
